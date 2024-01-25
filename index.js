872fc1c74ef035ba2f7b1258adc8af73353e2c2f
Uttam Bhagaur <uttambhagaur@gmail.com>
2024-01-25 20:06:03 +0530

    mongoose connection

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express, { request, response } from "express";
 import routes from "./src/routes/crmRoutes";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:37017/CRMdb", { useNewurlParser: true });
+
+//body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
