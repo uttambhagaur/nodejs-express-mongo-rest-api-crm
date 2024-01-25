3be58718b1324328a9eb6a3977b83331f8eacee3
Uttam Bhagaur <uttambhagaur@gmail.com>
2024-01-25 20:21:19 +0530

    add contact Schema

---
+++ src/models/crmModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+export const ContactSchema = new Schema({
+  firstName: {
+    type: String,
+    required: "Enter a first Name",
+  },
+  lastName: {
+    type: String,
+    required: "Enter a last Name",
+  },
+  email: {
+    type: String,
+  },
+  company: {
+    type: String,
+  },
+  phone: {
+    type: Number,
+  },
+  created_date: {
+    type: date,
+    default: Date.now,
+  },
+});
